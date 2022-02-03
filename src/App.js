8baf614e60ba7ce01b219dc983f26a51a730e869
EvgeniBlag <blagodarni@gmail.com>
2022-02-03 10:41:26 +0100

    add img

--- src/App.js
+++ src/App.js
@@ -3,9 +3,40 @@
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="wrapper">
+
+<header>
+  <div className="headerLeft">
+
+    <img width={40} height={40} src="/img/logo.png" alt="logo"/>
+
+    <div className="headerInfo">
+      <h3>React Sneakers</h3>
+      <p>Shop the beast sneakers</p>
     </div>
+  </div>
+
+  <ul className="headerRight">
+
+    <li>
+      <img width={18} height={12,27} src="/img/Vector.png" alt="cart"/>
+      <span>120$</span>
+    </li>
+
+    <li>
+      <svg/>
+    </li>
+
+  </ul>
+</header>
+
+<div className="content">
+
+  <h1>All sneakers</h1>
+  .......
+</div>
+</div>
+
   );
 }
 
