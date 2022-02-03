6a3ce1074970b404ec969aa1152481c493235c30
EvgeniBlag <blagodarni@gmail.com>
2022-02-03 14:33:18 +0100

    create content page and cardItems

--- src/App.js
+++ src/App.js
@@ -3,37 +3,132 @@
 
 function App() {
   return (
-    <div className="wrapper">
+    <div className="wrapper clear">
 
-<header>
-  <div className="headerLeft">
+<header className="d-flex  justify-between align-center p-40" >
+  <div className="d-flex align-center">
 
     <img width={40} height={40} src="/img/logo.png" alt="logo"/>
 
-    <div className="headerInfo">
-      <h3>React Sneakers</h3>
+    <div >
+      <h3 className="text-uppercase">React Sneakers</h3>
       <p>Shop the beast sneakers</p>
     </div>
   </div>
 
-  <ul className="headerRight">
+  <ul className="d-flex">
 
-    <li>
-      <img width={18} height={12,27} src="/img/Vector.png" alt="cart"/>
-      <span>120$</span>
+    <li className="mr-30">
+      <img width={18} height={18} src="/img/cart.png" alt="cart"/>
+      <span>
+       120$
+        </span>
     </li>
 
     <li>
-      <svg/>
+      <img width={20} height={20} src="/img/user.png" alt="user"/>
     </li>
 
   </ul>
 </header>
 
-<div className="content">
+<div className="content p-40">
 
-  <h1>All sneakers</h1>
-  .......
+  <h1 className="mb-40">All sneakers</h1>
+
+<div className="d-flex">
+
+
+<div className="card">
+  <img width={133} height={112} src="/img/sneakers.jpg" alt="sneakers"/>
+  <h5>
+  Sneakers for man Nike Blazer Mid Suede
+  </h5>
+ <div className=" d-flex justify-between align-center">
+   <div className="d-flex flex-column ">
+  <span>
+     price:
+  </span>      
+     <b>
+       120$
+     </b>
+   </div>
+   <button className="button">
+     <img width={11} height={11} src="/img/plus.png" alt="plus"/>
+   </button>
+ </div>
+  </div>
+
+
+  <div className="card">
+<img width={133} height={112} src="/img/sneakers2.jpg" alt="sneakers"/>
+<h5>
+Sneakers for man Nike Blazer Mid Suede
+</h5>
+<div className=" d-flex justify-between align-center">
+ <div className="d-flex flex-column ">
+<span>
+   price:
+</span>    
+   <b>
+     120$
+   </b>
+ </div>
+ <button className="button">
+   <img width={11} height={11} src="/img/plus.png" alt="plus"/>
+ </button>
+</div>
+</div>
+
+
+<div className="card">
+<img width={133} height={112} src="/img/sneakers3.jpg" alt="sneakers"/>
+<h5>
+Sneakers for man Nike Blazer Mid Suede
+</h5>
+<div className=" d-flex justify-between align-center">
+ <div className="d-flex flex-column ">
+<span>
+   price:
+</span>    
+   <b>
+     120$
+   </b>
+ </div>
+ <button className="button">
+   <img width={11} height={11} src="/img/plus.png" alt="plus"/>
+ </button>
+</div>
+</div>
+
+
+<div className="card">
+<img width={133} height={112} src="/img/sneakers4.jpg" alt="sneakers"/>
+<h5>
+Sneakers for man Nike Blazer Mid Suede
+</h5>
+<div className=" d-flex justify-between align-center">
+ <div className="d-flex flex-column ">
+<span>
+   price:
+</span>    
+   <b>
+     120$
+   </b>
+ </div>
+ <button className="button">
+   <img width={11} height={11} src="/img/plus.png" alt="plus"/>
+ </button>
+</div>
+</div>
+
+
+
+
+
+
+
+</div>
 </div>
 </div>
 
