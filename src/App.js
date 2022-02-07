b6e8f0f23e1dbe1e3e3cc017a084fdb5cea2b530
EvgeniBlag <blagodarni@gmail.com>
2022-02-07 13:32:42 +0100

    add cart elements

--- src/App.js
+++ src/App.js
@@ -10,8 +10,14 @@ function App() {
       <div className="drawer ">
   
 
-        <h2 className="mb-30">
+        <h2 className=" d-flex justify-between mb-30 ">
           Cart
+          <img className="removeBtn cu-p"
+             width={32}
+             height={32}
+             src="/img/btnRemove.png"
+             alt="remove"
+                />
         </h2>
 
         <div className="items">
@@ -54,25 +60,104 @@ function App() {
     alt="remove"
                 />
         </div>
+        <div className="cartItem d-flex align-center mb-20">
+
+<img className=" cartItemImg mr-20"
+   src="/img/sneakers.jpg"
+   alt="sneakers"
+                 />
+
+<div className="mr-20">
+<p className="mb-5">Sneakers for man, Nike Air Max 270</p>
+<b>120$</b>
+</div>
+
+<img className="removeBtn"
+width={32}
+height={32}
+src="/img/btnRemove.png"
+alt="remove"
+       />
+        </div>
+        <div className="cartItem d-flex align-center mb-20">
+
+<img className=" cartItemImg mr-20"
+   src="/img/sneakers.jpg"
+   alt="sneakers"
+                 />
+
+<div className="mr-20">
+<p className="mb-5">Sneakers for man, Nike Air Max 270</p>
+<b>120$</b>
+</div>
+
+<img className="removeBtn"
+width={32}
+height={32}
+src="/img/btnRemove.png"
+alt="remove"
+       />
+        </div>
+        <div className="cartItem d-flex align-center mb-20">
+
+<img className=" cartItemImg mr-20"
+   src="/img/sneakers.jpg"
+   alt="sneakers"
+                 />
+
+<div className="mr-20">
+<p className="mb-5">Sneakers for man, Nike Air Max 270</p>
+<b>120$</b>
+</div>
+
+<img className="removeBtn"
+width={32}
+height={32}
+src="/img/btnRemove.png"
+alt="remove"
+       />
+         </div>
+         <div className="cartItem d-flex align-center mb-20">
+
+<img className=" cartItemImg mr-20"
+   src="/img/sneakers.jpg"
+   alt="sneakers"
+                 />
+
+<div className="mr-20">
+<p className="mb-5">Sneakers for man, Nike Air Max 270</p>
+<b>120$</b>
+</div>
+
+<img className="removeBtn"
+width={32}
+height={32}
+src="/img/btnRemove.png"
+alt="remove"
+       />
+         </div>
 
         </div>
-        <ul className="cartTotalBlock">
 
+
+     <div className="cartTotalBlock">
+
+        <ul>
           <li >
             <span>Total:</span>
             <div></div>
             <b>210$</b>
           </li>
 
-
-          <li >
+          <li>
           <span>IVA 5%:</span>
             <div></div>
             <b>115$</b>
           </li>
-
         </ul>
-     
+
+       <button className="greenButton">Checkout  <img src="/img/arrow.svg" alt="Arrow"/> </button> 
+        </div>
 
        
 
